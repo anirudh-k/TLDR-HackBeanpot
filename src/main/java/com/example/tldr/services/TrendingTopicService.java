2a1f0118876a80c70c5a40ef74b538470e9fc12e
anirudh-k <anirudh.kaushik0@gmail.com>
2019-02-09 03:36:58 -0500

    add update and delete

--- src/main/java/com/example/tldr/services/TrendingTopicService.java
+++ src/main/java/com/example/tldr/services/TrendingTopicService.java
@@ -4,9 +4,11 @@ import com.example.tldr.models.TrendingTopic;
 import com.example.tldr.repositories.TrendingTopicRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +37,20 @@ public class TrendingTopicService {
   public TrendingTopic createTrendingTopic(
           @RequestBody TrendingTopic trendingTopic) {
     return trendingTopicRepository.save(trendingTopic);
+  }
+
+  @PutMapping("/api/trendingtopics/{id}")
+  public TrendingTopic updateTrendingTopic(
+          @PathVariable("id") Integer id,
+          @RequestBody TrendingTopic trendingTopicUpdates) {
+    TrendingTopic trendingTopic = trendingTopicRepository.findTrendingTopicById(id);
+    trendingTopic.setTitle(trendingTopicUpdates.getTitle());
+    return trendingTopicRepository.save(trendingTopic);
+  }
+
+  @DeleteMapping("/api/trendingtopics/{id}")
+  public void deleteTrendingTopic(
+          @PathVariable("id") Integer id) {
+    trendingTopicRepository.deleteById(id);
   }
 }
